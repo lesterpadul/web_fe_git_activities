ca36e615737a1695b6c6252e4b09d6298a4c1c7f
Lester Padul <padullester@gmail.com>
2023-01-10 18:45:37 +0800

    deploy/dev: add changes to header1.php

--- header1.php
+++ header1.php
@@ -1,3 +1,19 @@
-<div>
-	<span>Home</span>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<video></video>
+
+	<footer>
+		<div>
+			<span>
+				<p>
+					<b>hello world</b>
+				</p>
+			</span>
+		</div>
+	</footer>
+</body>
+</html>
